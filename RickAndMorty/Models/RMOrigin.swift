daf10705087f5f9cd8db19df0130be1090e6b467
Brian Yeh <brianyeh97@gmail.com>
2023-01-13 20:39:11 -0500

    Added docstrins and API related files

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Chenyeh Yeh on 1/13/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,14 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Chenyeh Yeh on 1/13/23.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+  }
+
