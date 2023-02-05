9396f40bec8bc777e61533f5ffb7d2582c1b765e
Brian Yeh <brianyeh97@gmail.com>
2023-02-04 22:18:05 -0500

    Updated character Info viewmodel

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
@@ -5,18 +5,127 @@
 //  Created by Chenyeh Yeh on 2/3/23.
 //
 
-import Foundation
+import UIKit
 
 final class RMCharacterInfoCollectionViewCellViewModel {
-    public let value: String
-    public let title: String
+    private let type: `Type`
     
-    init (
-        value: String,
-        title: String
+    private let value: String
     
-    ) {
+    static let dateFormatter: DateFormatter = {
+        // Format
+        // 2017-11-04T18:48:46.250Z
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSZ"
+        formatter.timeZone = .current
+        return formatter
+    }()
+    
+    static let shortDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        formatter.timeZone = .current
+        return formatter
+    }()
+
+    
+    public var title: String{
+        type.displayTitle
+    }
+    
+    public var displayTitle: String{
+        if value.isEmpty { return "None" }
+
+        
+        if let date = Self.dateFormatter.date(from: value),
+            type == .created {
+            return Self.shortDateFormatter.string(from: date)
+        }
+        
+        return value
+    }
+    
+    public var iconImage: UIImage? {
+        return type.iconImage
+    }
+    
+    public var tintColor: UIColor {
+        return type.tintColor
+    }
+    
+    enum `Type`: String {
+        case status
+        case gender
+        case type
+        case species
+        case origin
+        case created
+        case location
+        case epsiodeCount
+        
+        
+        var tintColor: UIColor {
+            switch self {
+            case .status:
+                return .systemBlue
+            case .gender:
+                return .systemRed
+            case .type:
+                return .systemPurple
+            case .species:
+                return .systemGreen
+            case .origin:
+                return .systemOrange
+            case .created:
+                return .systemPink
+            case .location:
+                return .systemYellow
+            case .epsiodeCount:
+                return .systemMint
+            }
+        }
+            
+        var iconImage: UIImage? {
+            switch self {
+            case .status:
+                return UIImage(systemName: "bell")
+            case .gender:
+                return UIImage(systemName: "bell")
+            case .type:
+                return UIImage(systemName: "bell")
+            case .species:
+                return UIImage(systemName: "bell")
+            case .origin:
+                return UIImage(systemName: "bell")
+            case .created:
+                return UIImage(systemName: "bell")
+            case .location:
+                return UIImage(systemName: "bell")
+            case .epsiodeCount:
+                return UIImage(systemName: "bell")
+            }
+        }
+        
+        var displayTitle: String {
+            switch self {
+            case .status,
+                .gender,
+                .type,
+                .species,
+                .origin,
+                .created,
+                .location:
+                return rawValue.uppercased()
+            case .epsiodeCount:
+                return "EPISODE COUNT"
+            }
+        }
+    }
+    
+    
+    init (type: `Type`, value: String) {
         self.value = value
-        self.title = title
+        self.type = type
     }
 }
