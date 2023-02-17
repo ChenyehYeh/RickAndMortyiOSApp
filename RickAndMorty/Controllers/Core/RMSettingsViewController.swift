1bc87826c7d129a445f112c4fe90c8c0b3292c3f
Brian Yeh <brianyeh97@gmail.com>
2023-02-17 15:33:32 -0500

    Add prompt app rating

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,21 +5,16 @@
 //  Created by Chenyeh Yeh on 1/12/23.
 //
 
+import StoreKit
+import SafariServices
 import SwiftUI
 import UIKit
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
     
-    private let settingsSwiftUIController = UIHostingController(
-        rootView: RMSettingsView(
-            viewModel: RMSettingsViewViewModel(
-                cellViewModels: RMSettingsOption.allCases.compactMap({
-                    return RMSettingsCellViewModel(type: $0)
-                })
-            )
-        )
-    )
+    private let settingsSwiftUIController: UIHostingController<RMSettingsView>? = nil
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +24,18 @@ final class RMSettingsViewController: UIViewController {
     }
     
     private func addSwiftUIController() {
+        let settingsSwiftUIController = UIHostingController(
+            rootView: RMSettingsView(
+                viewModel: RMSettingsViewViewModel(
+                    cellViewModels: RMSettingsOption.allCases.compactMap({
+                        return RMSettingsCellViewModel(type: $0) { [weak self] option in
+                            self?.handleTap(option: option)
+                        }
+                    })
+                )
+            )
+        )
+        
         addChild(settingsSwiftUIController)
         settingsSwiftUIController.didMove(toParent: self)
         view.addSubview(settingsSwiftUIController.view)
@@ -41,8 +48,25 @@ final class RMSettingsViewController: UIViewController {
             settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
 
-        
         ])
+    }
+    
+    private func handleTap(option: RMSettingsOption) {
+        guard Thread.current.isMainThread else {
+            return
+        }
+        
+        if let url = option.targetUrl {
+            // Open website
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+            
+        } else if option == .rateApp {
+            // Show rating prompt
+            if let windowScene = self.view.window?.windowScene {
+                SKStoreReviewController.requestReview(in: windowScene)
+            }
+        }
     }
 }
 
