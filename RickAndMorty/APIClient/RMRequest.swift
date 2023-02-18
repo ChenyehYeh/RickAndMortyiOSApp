bafcf06a95ee632d6d85810c7f52df6a78a2a299
Brian Yeh <brianyeh97@gmail.com>
2023-02-17 21:40:30 -0500

    Begin Location Tab

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -130,5 +130,6 @@ final class RMRequest {
 extension RMRequest {
     static let listCharactersRequests = RMRequest(endpoint: .character)
     static let listEpisodesRequests = RMRequest(endpoint: .episode)
+    static let listLocationssRequests = RMRequest(endpoint: .location)
 
 }
